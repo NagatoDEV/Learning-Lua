b28ee7690046744263ac7488d52aef126478b7c4
Nagato <67494727+NagatoDEV@users.noreply.github.com>
2022-06-02 19:02:41 -0400

    concatenate-text
    concatenate-text

---
+++ samples/concatenate-text/main.lua
@@ -0,0 +1,8 @@
+-- Prints text to the screen
+--[[
+   This program
+   is a basic program
+   is to display some text on the screen.
+]]
+
+print("Hello " .. "Nagato!" .. " This is a sample lua application.")
