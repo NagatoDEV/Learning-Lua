dac7e648bcc90e1d95ee2bbf61fd5048956c6177
Nagato <67494727+NagatoDEV@users.noreply.github.com>
2022-06-02 23:35:40 -0400

    Add files via upload

---
+++ samples/data-types-variables/main.lua
@@ -0,0 +1,10 @@
+--[[
+    nil - Is utilized to represent the absence of a useful value.
+    number - Example: 1 1 24 0.1 44
+    string - 'hello' 'hello world'
+    boolean true false
+    table
+]]
+
+-- Declarations
+
